c9d15659b5b8e4dd256c8583875ddadfd173ea28
teshager <teshager8922@gmail.com>
2024-08-06 17:59:37 +0300

    the fibonacci series generator function using recursion

--- fibonancci.js
+++ fibonancci.js
@@ -1,9 +1,8 @@
 const fibonacciIteration=(number)=>{
 
     let fibonaciSequence=[];
-
     for( let i=0;i<number;i++){
-         if(i<=1) fibonaciSequence.push(i);
+         if(i<2) fibonaciSequence.push(i);
          else{
             fibonaciSequence.push(fibonaciSequence[i-2]+fibonaciSequence[i-1]);
          } 
@@ -13,4 +12,29 @@ const fibonacciIteration=(number)=>{
 
 }
 
-console.log(fibonacciIteration(14));
+const fibonacciRecursive=(number)=>{
+let fibonacciSequence=[];
+
+  for( let i=0;i<number;i++){
+    fibonacciSequence.push(fibonacci(i));
+     
+ }
+
+ function fibonacci(num){
+    if(num<2) return num;
+    else{
+      return fibonacci(num-1)+fibonacci(num-2);
+    }
+      
+  }   
+  return fibonacciSequence;
+
+}
+   
+
+
+
+
+
+// console.log(fibonacciIteration(14));
+console.log(fibonacciRecursive(14));
